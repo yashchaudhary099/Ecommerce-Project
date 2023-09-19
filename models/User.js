2859f7b4b78f4e5176fdb359e5ca909debd2195e
yashchaudhary099 <yashchaudhary19999@gmail.com>
2023-09-19 13:42:37 +0530

    final commit 3

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const passportLocalMongoose = require('passport-local-Mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const userSchema = new mongoose.Schema({
      email:{
